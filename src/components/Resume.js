72755ef37a92a16f011cc6f7dfb0e6cd4a3842b2
Amanda Anderson <aanderson_120@yahoo.com>
2020-12-14 23:05:04 -0600

    update resume and projects

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ import '../images/AmandaAnderson.pdf'
 function Resume() {
     return (
       // <button class="Resume"> VIEW MY RESUME</button>
-      <a className="Resume" href="https://docs.google.com/document/d/1QYShnyDy1jv03UWFjBtn4spgfee4KrAClANVURbMj0A/edit?usp=sharing" rel="noopener noreferrer" target="_blank">VIEW MY RESUME</a>
+      <a className="Resume" href="https://docs.google.com/document/d/1kfDwdQMjrwTewg7vYfrKiU5eoFJTDHTiKqkKbS9GxTQ/edit?usp=sharing" rel="noopener noreferrer" target="_blank">VIEW MY RESUME</a>
     );
   }
   
